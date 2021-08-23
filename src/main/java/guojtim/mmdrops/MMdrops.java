7ef59d6c569310287f0a7cf372e1a838d0969243
GuoJTim <bnpiglike@gmail.com>
2021-08-24 00:24:25 +0800

    First Update

---
+++ src/main/java/guojtim/mmdrops/MMdrops.java
@@ -0,0 +1,17 @@
+package guojtim.mmdrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MMdrops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //test
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
